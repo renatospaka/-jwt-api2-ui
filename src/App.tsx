5596a3d471423267fcf6bacae5f4b90d0ded48fc
Renato Spakauskas <renatospaka@gmail.com>
2021-07-25 11:43:28 -0300

    feat: :sparkles: login page
    only html page, not real functionality implemented yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Home</a>
+          <div>
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Login</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Register</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main className="form-signin">
+        <form>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="name@example.com"
+            required
+          />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            required
+          />
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
