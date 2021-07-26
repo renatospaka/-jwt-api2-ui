8f8b63dd1e70c23ef0c3b3106bd2bd49dcfe9bdc
Renato Spakauskas <renatospaka@gmail.com>
2021-07-25 21:06:30 -0300

    feat: :sparkles: login page
    login page communicating to the go server and  redirecting logged users to the users's page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import { SyntheticEvent, useState } from "react";
+import { Redirect } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      credentials: "include",   //get the authentication cookie and use
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        email,
+        password,
+      })
+    });
+
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Redirect to="/" />
+  }
+  
   return (
-    <form>
+    <form onSubmit={submit}>
       <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-      <input type="email" className="form-control" placeholder="name@example.com" required />
-      <input type="password" className="form-control" placeholder="Password" required />
+      <input type="email" className="form-control" placeholder="name@example.com" required
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input type="password" className="form-control" placeholder="Password" required
+        onChange={e => setPassword(e.target.value)}
+      />
 
       <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
     </form>
